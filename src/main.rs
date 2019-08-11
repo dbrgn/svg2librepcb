2414b0a58d1807da3713d972e8c97fd6b610efa3
Danilo Bargen <mail@dbrgn.ch>
2019-08-12 01:33:39 +0200

    Prepare binary for Inkscape extension
    - Add more parameters
    - Write package directly to output path instead of stdout
    - Echo SVG to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#![allow(clippy::too_many_arguments)]
+
 use std::{
-    fs::read_to_string,
+    fs::{self, read_to_string},
     path::{Path, PathBuf},
+    process::exit,
 };
 
 use anyhow::{Context, Result};
@@ -14,24 +17,47 @@ use uuid::Uuid;
 struct Args {
     /// The SVG file to load
     svgfile: PathBuf,
+
+    /// Output path
+    #[clap(long, help_heading = "DIRECTORIES")]
+    outpath: PathBuf,
+
     /// Resulting LibrePCB package name
-    #[clap(short)]
+    #[clap(long, help_heading = "METADATA")]
     name: String,
     /// Resulting LibrePCB package description
-    #[clap(short)]
+    #[clap(long, default_value = "", help_heading = "METADATA")]
     description: String,
     /// Resulting LibrePCB package author
-    #[clap(short)]
+    #[clap(long, help_heading = "METADATA")]
     author: String,
+    /// Resulting LibrePCB package version
+    #[clap(long, default_value = "0.1.0", help_heading = "METADATA")]
+    version: String,
+    /// Resulting LibrePCB package keywords
+    #[clap(long, default_value = "", help_heading = "METADATA")]
+    keywords: String,
+
+    /// Resulting LibrePCB package UUID [default: random]
+    #[clap(long, help_heading = "UUIDS")]
+    uuid_pkg: Option<String>,
     /// Resulting LibrePCB package category UUID
-    #[clap(short = 'c')]
-    pkgcat_uuid: String,
-    /// Resulting LibrePCB package UUID (optional)
-    #[clap(short = 'u')]
-    pkg_uuid: Option<String>,
-    /// Resulting LibrePCB package keywords (optional)
-    #[clap(short)]
-    keywords: Option<String>,
+    #[clap(long, help_heading = "UUIDS")]
+    uuid_pkgcat: String,
+
+    /// Generate copper layer
+    #[clap(long, default_value = "true", help_heading = "LAYERS")]
+    layer_copper: bool,
+    /// Generate placement layer
+    #[clap(long, default_value = "true", help_heading = "LAYERS")]
+    layer_placement: bool,
+    /// Generate stop mask layer
+    #[clap(long, default_value = "true", help_heading = "LAYERS")]
+    layer_stopmask: bool,
+
+    /// Passed in by Inkscape, ignored, not currently supported
+    #[clap(long, hide(true))]
+    id: Option<Vec<String>>,
 }
 
 fn make_uuid() -> Uuid {
@@ -68,7 +94,7 @@ fn make_footprint(
 }
 
 fn make_package(
-    uuid: Option<&str>,
+    uuid: &str,
     name: &str,
     description: &str,
     keywords: &str,
@@ -78,11 +104,7 @@ fn make_package(
     footprints: &[Vec<String>],
 ) -> Vec<String> {
     let mut lines: Vec<String> = vec![];
-    lines.push(format!(
-        r#"(librepcb_package {}"#,
-        uuid.map(|u| u.to_string())
-            .unwrap_or_else(|| make_uuid().to_string()),
-    ));
+    lines.push(format!(r#"(librepcb_package {}"#, uuid));
     lines.push(format!(r#" (name "{}")"#, name));
     lines.push(format!(r#" (description "{}")"#, description));
     lines.push(format!(r#" (keywords "{}")"#, keywords));
@@ -103,29 +125,70 @@ fn make_package(
 fn main() -> Result<()> {
     let args = Args::parse();
 
+    // Load and parse SVG
     let svg_string = load_svg(&args.svgfile).context("Could not read SVG file")?;
     let polylines = svg2polylines::parse(&svg_string, 0.15).expect("Could not parse SVG file");
 
-    let footprints = [
-        make_footprint("top_placement", "Top Placement", "", &polylines),
-        make_footprint("top_cu", "Top Copper", "", &polylines),
-        make_footprint("top_stop_mask", "Top Stop Mask", "", &polylines),
-    ];
+    // Ensure that output library path exists
+    let lib_path = match args.outpath.canonicalize() {
+        Ok(path) => path,
+        Err(e) => {
+            eprintln!("Error: Invalid output path: {}", e);
+            exit(1);
+        }
+    };
+    if !lib_path.exists() {
+        eprintln!("Error: Output path {:?} does not exist", lib_path);
+        exit(1);
+    }
+    if !lib_path.is_dir() {
+        eprintln!("Error: Output path {:?} is not a directory", lib_path);
+        exit(1);
+    }
 
+    // Generate footprints
+    let mut footprints = vec![];
+    if args.layer_copper {
+        footprints.push(make_footprint("top_cu", "Top Copper", "", &polylines));
+    }
+    if args.layer_placement {
+        footprints.push(make_footprint(
+            "top_placement",
+            "Top Placement",
+            "",
+            &polylines,
+        ));
+    }
+    if args.layer_stopmask {
+        footprints.push(make_footprint(
+            "top_stop_mask",
+            "Top Stop Mask",
+            "",
+            &polylines,
+        ));
+    }
+
+    // Generate package
+    let uuid_pkg = args.uuid_pkg.unwrap_or_else(|| make_uuid().to_string());
     let pkg = make_package(
-        args.pkg_uuid.as_deref(),
+        &uuid_pkg,
         &args.name,
         &args.description,
         &args.author,
-        args.keywords.as_deref().unwrap_or(""),
-        "0.1.0",
-        &args.pkgcat_uuid,
+        &args.keywords,
+        &args.version,
+        &args.uuid_pkgcat,
         &footprints,
     );
 
-    for line in pkg {
-        println!("{}", line);
-    }
+    // Write package to library
+    let pkg_path = lib_path.join("pkg").join(&uuid_pkg);
+    fs::create_dir_all(&pkg_path).unwrap();
+    fs::write(pkg_path.join(".librepcb-pkg"), "0.1").unwrap();
+    fs::write(pkg_path.join("package.lp"), pkg.join("\n")).unwrap();
+
+    // Echo original SVG on stdout for compatibility with Inkscape.
+    println!("{}", svg_string);
 
     Ok(())
 }
