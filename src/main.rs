f41fffc8cd4e193d8a5c636dd25bfbf24e7b4d3b
Danilo Bargen <mail@dbrgn.ch>
2022-07-17 16:44:20 +0200

    Allow aligning resulting package (#5)

--- src/main.rs
+++ src/main.rs
@@ -58,6 +58,9 @@ struct Args {
     /// Flattening tolerance
     #[clap(long, default_value = "0.15", help_heading = "PARAMETERS")]
     flattening_tolerance: f64,
+    /// Align the centerpoint
+    #[clap(long, value_enum, default_value = "none", help_heading = "PARAMETERS")]
+    align: Align,
 
     /// Passed in by Inkscape, ignored, not currently supported
     #[clap(long, hide(true))]
@@ -72,32 +75,79 @@ fn load_svg(path: &Path) -> Result<String> {
     Ok(read_to_string(path)?)
 }
 
+#[derive(PartialEq, Eq, Copy, Clone, Debug, clap::ValueEnum)]
+enum Align {
+    None,
+    Center,
+    TopLeft,
+    BottomLeft,
+}
+
 fn make_footprint(
     layer: &str,
     name: &str,
     description: &str,
+    align: Align,
     polylines: &[Polyline],
 ) -> Vec<String> {
     let mut lines = vec![];
     lines.push(format!(r#"(footprint {}"#, make_uuid()));
     lines.push(format!(r#" (name "{}")"#, name));
     lines.push(format!(r#" (description "{}")"#, description));
-    for polyline in polylines {
-        let closed = polyline[0] == polyline[polyline.len() - 1];
-        let (width, fill) = match closed {
-            true => ("0.0", "true"),
-            false => ("0.2", "false"),
-        };
-        lines.push(format!(r#" (polygon "{}" (layer {})"#, make_uuid(), layer));
-        lines.push(format!(r#"  (width {}) (fill {}) (grab_area true)"#, width, fill));
-        for pair in polyline {
-            lines.push(format!(
-                r#"  (vertex (position {:.3} {:.3}) (angle 0.0))"#,
-                pair.x, -pair.y
-            ));
+
+    if !polylines.is_empty() {
+        // Note: In SVG, the top left point is (0, 0). The y-axis expands
+        //       downwards. In LibrePCB, the Y axis is the other way around, and
+        //       expands upwards.
+
+        // First, find bounds to allow centering
+        let first_pair = polylines[0][0];
+        let (mut x_min, mut x_max, mut y_min, mut y_max) =
+            (first_pair.x, first_pair.x, first_pair.y, first_pair.y);
+        for polyline in polylines {
+            for pair in polyline {
+                x_min = pair.x.min(x_min);
+                x_max = pair.x.max(x_max);
+                y_min = pair.y.min(y_min);
+                y_max = pair.y.max(y_max);
+            }
         }
-        lines.push(r#" )"#.to_string());
+
+        // Calculate offset (still in SVG coordinate mode)
+        let (dx, dy) = match align {
+            Align::None => (0.0, 0.0),
+            Align::Center => {
+                let halfwidth = (x_max - x_min) / 2.0;
+                let halfheight = (y_max - y_min) / 2.0;
+                (-x_min - halfwidth, -y_min - halfheight)
+            }
+            Align::TopLeft => (-x_min, -y_min),
+            Align::BottomLeft => (-x_min, -y_max),
+        };
+
+        // Then generate vertices
+        for polyline in polylines {
+            let closed = polyline[0] == polyline[polyline.len() - 1];
+            let (width, fill) = match closed {
+                true => ("0.0", "true"),
+                false => ("0.2", "false"),
+            };
+            lines.push(format!(r#" (polygon "{}" (layer {})"#, make_uuid(), layer));
+            lines.push(format!(
+                r#"  (width {}) (fill {}) (grab_area true)"#,
+                width, fill
+            ));
+            for pair in polyline {
+                lines.push(format!(
+                    r#"  (vertex (position {:.3} {:.3}) (angle 0.0))"#,
+                    pair.x + dx,
+                    -(pair.y + dy) // Invert axis
+                ));
+            }
+            lines.push(r#" )"#.to_string());
+        }
     }
+
     lines.push(r#")"#.to_string());
     lines
 }
@@ -119,7 +169,10 @@ fn make_package(
     lines.push(format!(r#" (keywords "{}")"#, keywords));
     lines.push(format!(r#" (author "{}")"#, author));
     lines.push(format!(r#" (version "{}")"#, version));
-    lines.push(format!(r#" (created {})"#, Utc::now().to_rfc3339().replace("+00:00", "Z")));
+    lines.push(format!(
+        r#" (created {})"#,
+        Utc::now().to_rfc3339().replace("+00:00", "Z")
+    ));
     lines.push(" (deprecated false)".to_string());
     lines.push(format!(r#" (category {})"#, pkgcat));
     for footprint in footprints {
@@ -136,7 +189,8 @@ fn main() -> Result<()> {
 
     // Load and parse SVG
     let svg_string = load_svg(&args.svgfile).context("Could not read SVG file")?;
-    let polylines = svg2polylines::parse(&svg_string, args.flattening_tolerance).expect("Could not parse SVG file");
+    let polylines = svg2polylines::parse(&svg_string, args.flattening_tolerance)
+        .expect("Could not parse SVG file");
 
     // Ensure that output library path exists
     let lib_path = match args.outpath.canonicalize() {
@@ -158,13 +212,20 @@ fn main() -> Result<()> {
     // Generate footprints
     let mut footprints = vec![];
     if args.layer_copper {
-        footprints.push(make_footprint("top_cu", "Top Copper", "", &polylines));
+        footprints.push(make_footprint(
+            "top_cu",
+            "Top Copper",
+            "",
+            args.align,
+            &polylines,
+        ));
     }
     if args.layer_placement {
         footprints.push(make_footprint(
             "top_placement",
             "Top Placement",
             "",
+            args.align,
             &polylines,
         ));
     }
@@ -173,6 +234,7 @@ fn main() -> Result<()> {
             "top_stop_mask",
             "Top Stop Mask",
             "",
+            args.align,
             &polylines,
         ));
     }
